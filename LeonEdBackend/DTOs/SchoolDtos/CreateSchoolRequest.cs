ee39fa141d36fa339394de3e632f933e148dfbaa
engr-Godswillochi <ochigodswill@gmail.com>
2026-05-02 13:13:16 +0100

    Add School model, CreateSchoolRequest DTO, DbContext, and Enums for application structure

--- LeonEdBackend/DTOs/SchoolDtos/CreateSchoolRequest.cs
+++ LeonEdBackend/DTOs/SchoolDtos/CreateSchoolRequest.cs
@@ -3,27 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LeonEdBackend.Models
+namespace LeonEdBackend.DTOs.SchoolDtos
 {
-    public class School
+    public class CreateSchoolRequest
     {
-        public Guid Id {get; set;}
-        public Guid UserId {get; set;}
         public string Name {get; set;} = string.Empty;
         public string Address {get; set;} = string.Empty;
         public string ContactEmail {get; set;} = string.Empty;
         public string ContactPhone {get; set;} = string.Empty;
         public string LogoUrl {get; set;} = string.Empty;
-        public string Slug {get; set;} = string.Empty;
-        public SubscriptionStatus SubscriptionStatus {get; set;}
-        public DateTime CreatedAt {get; set;}
-    }
-
-    public enum SubscriptionStatus
-    {
-        Free,
-        Trial,
-        Paid,
-        Expired
+        public string Password {get; set;} = string.Empty;
     }
 }
